796ec2c0bd8647269c811773a7a9ada46b5a1191
unknown <selvarajaa13@gmail.com>
2022-11-23 05:57:59 +0000

    first commit

--- git-sampl.bat
+++ git-sampl.bat
@@ -1,6 +1,5 @@
 @echo off
+git pull origin master
 git add .
 git commit -m "first commit"
-git remote add origin https://github.com/selvar2/devops-essentials-sample-app.git
-git push -u origin master
 pause
